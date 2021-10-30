246e4dc5e7741a8d888f5bbe137e4aa33d4b9b4a
William Ciesialka <wciesialka@gmail.com>
2021-10-30 13:18:27 -0400

    Updated Point dataclass.

--- SnakeGame/Point.py
+++ SnakeGame/Point.py
@@ -1,11 +1,24 @@
 from dataclasses import dataclass
 
+@dataclass(eq=True,order=False)
 class Point:
+    """
+    Represents a point in 2D space.
+
+    :field x: The x coordinate of the point.
+    :type x: int
+    :field y: The y coordinate of the point.
+    :type y: int
+    """
 
     x: int
     y: int
 
     def copy(self):
+        '''Returns a copy of the point object.
+        
+        :returns copy: Copy of the point.
+        :rtype: Point'''
         return Point(self.x,self.y)
 
     def __add__(self,other):
